158655790b984e272fcb0b418d5608155c3ca8be
Alastair Zotos <alastairzotos@gmail.com>
2023-11-22 19:30:35 +0200

    feat: use react@16.12.0 as peer dep

--- apps/example-client/src/pages/_app.tsx
+++ apps/example-client/src/pages/_app.tsx
@@ -11,15 +11,6 @@ export default function App({ Component, pageProps }: AppProps) {
       apiUrl='http://localhost:3001'
       onRegister={() => router.push('/register')}
       onLogin={() => router.push('/')}
-      // theme={extendPersonaTheme({
-      //   backgroundColor: '#d0ffcc',
-      //   brandColor: '#9c0aa8',
-      //   textColor: 'black',
-      //   buttonTextColor: 'white',
-      //   // showOutline: false,
-      //   borderRadius: 0,
-      //   fontFamily: 'Papyrus',
-      // })}
     >
       <main className="p-24" style={{ width: 600 }}>
         <Component {...pageProps} />
