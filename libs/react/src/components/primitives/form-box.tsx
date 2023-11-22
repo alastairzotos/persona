b4a198b0c8e230e8b2785defe2df5c7659581c55
Alastair Zotos <alastairzotos@gmail.com>
2023-11-22 20:58:57 +0200

    fix: full width forms

--- libs/react/src/components/primitives/form-box.tsx
+++ libs/react/src/components/primitives/form-box.tsx
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 
 export const FormBox = styled('div')(({ theme }) => ({
+  width: '100%',
   padding: theme.pad * 8,
   backgroundColor: theme.backgroundColor,
   border: theme.showOutline ? `1px solid ${theme.brandColor}` : 'none',
   borderTop: theme.showOutline ? `4px solid ${theme.brandColor}` : 'none',
   borderRadius: theme.borderRadius,
-  fontFamily: `${theme.fontFamily}`
+  fontFamily: `${theme.fontFamily}`,
 }))
