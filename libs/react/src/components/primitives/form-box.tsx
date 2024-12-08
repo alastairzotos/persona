0294b7c09112d43b44f2dc02fd8fa172405f7261
Alastair Zotos <alastair.zotos@persado.com>
2024-12-08 15:14:18 +0200

    Remove padding

--- libs/react/src/components/primitives/form-box.tsx
+++ libs/react/src/components/primitives/form-box.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const FormBox = styled('div')(({ theme }) => ({
   width: '100%',
-  padding: theme.pad * 8,
   backgroundColor: theme.backgroundColor,
   fontFamily: `${theme.fontFamily}`,
 }))
